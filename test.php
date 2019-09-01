dc68a7592169864408fb769c76d713e3b408c5d0
claudito <1107000044@upci.edu.pe>
2019-09-01 16:02:22 -0500

    print

--- test.php
+++ test.php
@@ -2,6 +2,8 @@
 
 include'vendor/autoload.php';
 
+date_default_timezone_set('America/Lima');
+
 use Mike42\Escpos\PrintConnectors\FilePrintConnector;
 use Mike42\Escpos\Printer;
 
@@ -11,8 +13,27 @@ $connector = new FilePrintConnector("\\\\192.168.1.6\\BIXOLON-imp");
 //Conexión al Recurso(Impresora)
 $printer = new Printer($connector);
 
+//Datos de Impresión
+
+$cab = json_decode($_REQUEST['cab'],TRUE);
+
+//var_dump($cab);
+
+
 //Enviar los Datos de Impresión
-$printer -> text("Impresión de Prueba\n");
+$printer -> setJustification(Printer::JUSTIFY_CENTER);
+$printer -> text("POLLERIA EL FOGON\n");
+$printer -> text("VENTA N-".$cab['id']."\n");
+$printer -> text(date('d/m/Y H:i:s')."\n");
+$printer -> setJustification(); // Reset
+$printer -> text("Cantidad:\n");
+$printer -> text($cab['cantidad']."\n");
+$printer -> text("Producto:\n");
+$printer -> text($cab['nombre']."\n");
+$printer -> text("Precio:\n");
+$printer -> text($cab['precio_unidad']."\n");
+$printer -> text("Total:\n");
+$printer -> text($cab['total']."\n");
 
 $printer -> cut();
 $printer -> close();
